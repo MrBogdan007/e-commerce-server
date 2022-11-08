1a2af96eb4c03b4808635844c08e3a1d52e7485d
Bohdan <bogdanbrohovetsky@gmail.com>
2022-11-08 12:46:00 +0200

    added seraching

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,15 +1,17 @@
-
-import { Pagination,useTheme } from "@mui/material";
+import { Pagination, useTheme } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 import { addCartItem } from "../redux/reducers/cartReducer";
-import {setOffsetReducer} from "../redux/reducers/productOffset"
-import { fetchCategory, fetchPagination, fetchProducts,  } from "../redux/reducers/productReducer";
-import {  singleProduct } from "../redux/reducers/singleProductReducer";
+import { setOffsetReducer } from "../redux/reducers/productOffset";
+import {
+  fetchCategory,
+  fetchPagination,
+  fetchProducts,
+} from "../redux/reducers/productReducer";
+import { singleProduct } from "../redux/reducers/singleProductReducer";
 import { Product } from "../types/product";
-
 
 import Modal from "./interface/Modal";
 import NavBar from "./NavBar";
@@ -19,71 +21,119 @@ import SingleProduct from "./SingleProduct";
 
 const Category = () => {
   const fetchPaginationValue = useAppSelector((state) => state.productReducer);
-  
+
   // const products = useAppSelector((state) => state.productAllReducer);
-  const [products, setProducts] = useState<Product[]>([])
-  const [offset,setOffset] = useState(0);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [select, setSelect] = useState('');
+  const [offset, setOffset] = useState('0');
   const dispatch = useAppDispatch();
   // useEffect(() => {
   //   dispatch(fetchPagination(offset));
   // },[offset])
+  const [search,setSearch] = useState('');
+  const tempList =  products.filter(item => item.title.includes(search))
 
-  const theme = useTheme()
-  const navigate = useNavigate()
-  const detailsShow = (id:number) => {
-    console.log('clicked');
-    navigate(`/category/${id}`)
-    dispatch(singleProduct(id))
+  
+
+  const theme = useTheme();
+  const navigate = useNavigate();
+  const detailsShow = (id: number) => {
+    console.log("clicked");
+    navigate(`/category/${id}`);
+    dispatch(singleProduct(id));
+  };
+
+  const categoryAll = () => {
+    dispatch(fetchProducts());
+    navigate(`/category`);
+  };
+  const categoryCloth = () => {
+    dispatch(fetchCategory(1));
+  };
+  const categoryElectronics = () => {
+    dispatch(fetchCategory(2));
+  };
+  const categoryFurniture = () => {
+    dispatch(fetchCategory(3));
+  };
+  const categoryShoes = () => {
+    dispatch(fetchCategory(4));
+  };
+  const categoryOthers = () => {
+    dispatch(fetchCategory(5));
+  };
+
+  const addToCart = (
+    id: number,
+    title: string,
+    price: number,
+    image: string
+  ) => {
+    dispatch(addCartItem({ id: id, title: title, price: price, image: image }));
+  };
+
+  const inputHandler = (e:any) => {
+    setSearch(e);
   }
-
-const categoryAll = () => {
-  dispatch(fetchProducts());
-  navigate(`/category`)
-}
-const categoryCloth = () => {
-  dispatch(fetchCategory(1));
-}
-const categoryElectronics = () => {
-  dispatch(fetchCategory(2));
-}
-const categoryFurniture = () => {
-  dispatch(fetchCategory(3));
-}
-const categoryShoes = () => {
-  dispatch(fetchCategory(4));
-}
-const categoryOthers = () => {
-  dispatch(fetchCategory(5));
-}
-
-const addToCart = (id:number,title:string,price:number,image:string) => {
-  dispatch(addCartItem({id:id,title:title,price:price,image:image}))
-}
+  const handleChange = (e) => {
+    setSelect(e.target.value); 
+  }
+  console.log(select);
+  
   return (
     <>
-    
-      <NavbarOther/>
+      <NavbarOther />
       <div className="container">
-      <div className="product product-fetch">
-        {products.map((item) => (
-          <div key={item.id} className="product-item">
-            {" "}
-            <div className="product__title">{item.title}</div>{" "}
-            <div className="product__price">{`${item.price}$`} </div>{" "}
-            <div className="product__image">
-              {<img src={item.images[0]} alt="shoes" />}
-            </div>
-            <button  onClick={() => addToCart(item.id,item.title,item.price,item.images[0])} className="product__button">Add to cart</button>
-            <button onClick={()=> detailsShow(item.id)} className="product__button">Details</button>
+        <div className="product-header">
+          <div className="product-search">
+            <input onChange={(e) => inputHandler(e.target.value)} className="product__input" type="text" />
+              <button className="button">Search</button>{" "}
           </div>
-          
-        ))}
-      </div>
+
+          <div className="product__select">
+            <select name="properties" id="properties" onChange={handleChange}>
+              {" "}
+              <option selected value="naming">by naming</option>{" "}
+              <option value="cheap">from cheap</option>{" "}
+              <option value="expensive">from expensive</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="product product-fetch">
+          {tempList.map((item) => (
+            <div key={item.id} className="product-item">
+              {" "}
+              <div className="product__title">{item.title}</div>{" "}
+              <div className="product__price">{`${item.price}$`} </div>{" "}
+              <div className="product__image">
+                {<img src={item.images[0]} alt="shoes" />}
+              </div>
+              <button
+                onClick={() =>
+                  addToCart(item.id, item.title, item.price, item.images[0])
+                }
+                className="product__button"
+              >
+                Add to cart
+              </button>
+              <button
+                onClick={() => detailsShow(item.id)}
+                className="product__button"
+              >
+                Details
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="product-category">
-        <ul className="product-ul" style={{ color: theme.palette.mode === "light" ? "black"  : "black"}}>
+        <ul
+          className="product-ul"
+          style={{ color: theme.palette.mode === "light" ? "black" : "black" }}
+        >
           <li onClick={() => categoryAll()}>All products</li>
-          <li onClick={() => categoryCloth()} >Clothes</li>
+          <li onClick={() => categoryCloth()}>Clothes</li>
           <li onClick={() => categoryElectronics()}>Electronics</li>
           <li onClick={() => categoryFurniture()}>Furniture</li>
           <li onClick={() => categoryShoes()}>Shoes</li>
@@ -91,9 +141,8 @@ const addToCart = (id:number,title:string,price:number,image:string) => {
         </ul>
       </div>
       <div className="pagination">
-      <PaginationCustom setProducts={setProducts}/>
+        <PaginationCustom setProducts={setProducts} />
       </div>
-    
     </>
   );
 };
